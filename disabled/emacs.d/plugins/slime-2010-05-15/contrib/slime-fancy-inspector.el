1470223b3f9a6741f2d6c42d9402d68f0446b6e6
Chap Lovejoy <chaplovejoy@gmail.com>
2011-07-25 16:57:35 -0400

    Remove old emacs packages

--- disabled/emacs.d/plugins/slime-2010-05-15/contrib/slime-fancy-inspector.el
+++
@@ -1,6 +0,0 @@
-
-(define-slime-contrib slime-fancy-inspector
-  "Fancy inspector for CLOS objects."
-  (:authors "Marco Baringer <mb@bese.it> and others")
-  (:license "GPL")
-  (:swank-dependencies swank-fancy-inspector))
